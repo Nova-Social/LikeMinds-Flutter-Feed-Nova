99da648ce39d6b64885d7309f3129ceedd4f9b54
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2023-10-09 15:42:02 +0530

    added tap listener on user tile

--- lib/src/widgets/user_tile_widget.dart
+++ lib/src/widgets/user_tile_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:likeminds_feed/likeminds_feed.dart';
 import 'package:likeminds_feed_nova_fl/likeminds_feed_nova_fl.dart';
 import 'package:likeminds_feed_nova_fl/src/services/likeminds_service.dart';
-import 'package:likeminds_feed_nova_fl/src/utils/constants/ui_constants.dart';
 import 'package:likeminds_feed_ui_fl/likeminds_feed_ui_fl.dart';
 
 // TODO: Customisation
@@ -21,36 +20,47 @@ class LMUserTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        LMProfilePicture(
-          size: imageSize ?? 50,
-          fallbackText: user.name,
-          onTap: () {
-            if (user.sdkClientInfo != null) {
-              locator<LikeMindsService>()
-                  .routeToProfile(user.sdkClientInfo!.userUniqueId);
-            }
-          },
-          imageUrl: user.imageUrl,
-          boxShape: BoxShape.circle,
+    return GestureDetector(
+      onTap: () {
+        if (user.sdkClientInfo != null) {
+          locator<LikeMindsService>()
+              .routeToProfile(user.sdkClientInfo!.userUniqueId);
+        }
+      },
+      child: Container(
+        color: Colors.transparent,
+        child: Row(
+          children: [
+            LMProfilePicture(
+              size: imageSize ?? 50,
+              fallbackText: user.name,
+              onTap: () {
+                if (user.sdkClientInfo != null) {
+                  locator<LikeMindsService>()
+                      .routeToProfile(user.sdkClientInfo!.userUniqueId);
+                }
+              },
+              imageUrl: user.imageUrl,
+              boxShape: BoxShape.circle,
+            ),
+            kHorizontalPaddingLarge,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  titleText ??
+                      LMTextView(
+                        text: user.name,
+                        textStyle: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                  kVerticalPaddingMedium,
+                  subText ?? const SizedBox(),
+                ],
+              ),
+            ),
+          ],
         ),
-        kHorizontalPaddingLarge,
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              titleText ??
-                  LMTextView(
-                    text: user.name,
-                    textStyle: Theme.of(context).textTheme.bodyLarge,
-                  ),
-              kVerticalPaddingMedium,
-              subText ?? const SizedBox(),
-            ],
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
